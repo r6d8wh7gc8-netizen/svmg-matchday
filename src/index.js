e8a74d111ee3a72d3b4942c46da36e65808c00ac
r6d8wh7gc8-netizen <r6d8wh7gc8@privaterelay.appleid.com>
2026-07-04 08:41:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-;import App from './App
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root =
-  React DOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App></React.StrictMode>):
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
